2e69c8dec71f8ccff13ac93b025b74cc79367408
salah <salahalhimyari2023@gmail.com>
2025-08-03 01:02:56 +0300

    chore: add initial main.js file

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+if (localStorage.getItem('theme') === 'dark' || 
+    (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.toggle('dark');
+}
